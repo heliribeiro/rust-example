e2afa2fd7d2fbab9ad6639f92df7670eb8e2b290
Heli <heliribeiro2@gmail.com>
2024-03-30 06:33:25 -0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use std::io;
+fn main() {
+    println!("Digite um multiplicador");
+    let mut number = String::new();
+
+    io::stdin()
+        .read_line(&mut number)
+        .expect("Failed to read line");
+
+    let multi: i32 = number
+        .trim()
+        .parse()
+        .expect("error when converting to number");
+
+    for number in 0..=9 {
+        let result = number * multi;
+        println!("{number} x {multi} = {}", result);
+    }
+}
